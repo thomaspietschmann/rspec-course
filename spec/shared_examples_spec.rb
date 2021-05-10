792530e35e283edd833fa1aa4c36ec0ada41c46a
Thomas Pietschmann <thomas@pietschi.com>
2021-05-10 18:21:53 +0200

    shared examples

---
+++ spec/shared_examples_spec.rb
@@ -0,0 +1,33 @@
+# all these subjects will return a length of 3, so we'll use a shared example
+
+RSpec.shared_examples 'a Ruby object with a length of 3' do
+  it 'returns the number of items' do
+    expect(subject.length).to eq(3)
+  end
+end
+
+RSpec.describe Array do
+  subject { [1, 2, 3] }
+  include_examples 'a Ruby object with a length of 3'
+end
+
+RSpec.describe String do
+  subject { 'abc' }
+  include_examples 'a Ruby object with a length of 3'
+end
+
+RSpec.describe Hash do
+  subject { { a: 1, b: 2, c: 3 } }
+  include_examples 'a Ruby object with a length of 3'
+end
+
+class SausageLink
+  def length
+    3
+  end
+end
+
+RSpec.describe SausageLink do
+  subject { described_class.new }
+  include_examples 'a Ruby object with a length of 3'
+end
