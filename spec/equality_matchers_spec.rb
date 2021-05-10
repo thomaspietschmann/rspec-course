f7525261b955345bc1b1cf2d9b5dac8d7136ac6e
Thomas Pietschmann <thomas@pietschi.com>
2021-05-10 17:00:56 +0200

    equal matcher

--- spec/equality_matchers_spec.rb
+++ spec/equality_matchers_spec.rb
@@ -18,4 +18,20 @@ RSpec.describe 'equality matchers' do
       expect(a).to eq(3.0)
     end
   end
+
+  describe 'equal and be matcher' do
+    let(:c) { [1, 2, 3] }
+    let(:d) { [1, 2, 3] }
+    let(:e) { c }
+
+    it 'cares about object identity' do
+      expect(c).to eq(d)
+      expect(c).to eql(d)
+      expect(c).to equal(e)
+      expect(c).to be(e)
+      expect(c).not_to equal(d)
+      expect(c).not_to be(d)
+      expect(c).not_to equal([1, 2, 3])
+    end
+  end
 end
