ecd770c4c957e413c3806ea3272fc3d00171b939
Thomas Pietschmann <thomas@pietschi.com>
2021-05-10 18:57:50 +0200

    contain_exactly matcher

---
+++ spec/contain_exactly_matcher_spec.rb
@@ -0,0 +1,15 @@
+RSpec.describe 'contain_exactly matcher' do
+  subject { [1, 2, 3] }
+
+  describe 'long form syntax' do
+    it 'should check for presence of all elements' do
+      expect(subject).to contain_exactly(1, 2, 3)
+      expect(subject).to contain_exactly(2, 3, 1)
+      expect(subject).to contain_exactly(3, 1, 2)
+      # expect(subject).to contain_exactly(3, 1) --> false
+      # expect(subject).to contain_exactly(1, 2, 3, 4) --> false
+    end
+  end
+
+  it { is_expected.to contain_exactly(1, 2, 3) }
+end
